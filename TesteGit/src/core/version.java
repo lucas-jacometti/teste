ff9fca031a7b761f960e4b6d685451bde8598871
lucas-jacometti <lucas_jacometti@hotmail.com>
2012-08-08 11:17:43 -0300

    teste1

--- TesteGit/src/core/version.java
+++ TesteGit/src/core/version.java
@@ -5,7 +5,7 @@ public class version {
 	private static final String VERSION = "1";
 	private static final String DISTRIBUTION = "0";
 	private static final String RELEASE = "0";
-	private static final String BUILD = "0";
+	private static final String BUILD = "1";
 	
 	public static String getVersion(){
 		return VERSION + "." + DISTRIBUTION + "." + RELEASE + "." + BUILD;
